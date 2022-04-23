87a674ee00dd69415c76811b073c8668b66f1bec
Babdus <babdusi@gmail.com>
2022-04-23 17:51:23 +0400

    Finish adding numerals

--- generate_ipa_section.py
+++ generate_ipa_section.py
@@ -1,4 +1,6 @@
 import sys
+import pyperclip
+
 
 letters_to_ipa = [
     ('sh', '&#643;'),
@@ -25,6 +27,7 @@ def generate_ipa_section(word, n_tabs=0):
     ipa_word = word_to_ipa(word)
     tabs = '    '*n_tabs
     html = f'{tabs}<a class="tooltip" href="#word-{word}"><span class="ipa">{ipa_word}</span></a>\n{tabs}<audio id="audio-{word}"><source src="audio/{word}.mp3" type="audio/mp3"></audio>\n{tabs}<button class="play-button" onclick="document.getElementById(\'audio-{word}\').play()">&#9658;</button>'
+    pyperclip.copy(html)
     return html
 
 print(generate_ipa_section(sys.argv[1], int(sys.argv[2]) if len(sys.argv) > 2 else 0))
