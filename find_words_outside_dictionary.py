3c507ce1500424ba516e6218bfae7de63c4846d2
babdus <babdusi@gmail.com>
2023-02-12 04:28:25 +0400

    sort missing words from dictionary

--- find_words_outside_dictionary.py
+++ find_words_outside_dictionary.py
@@ -1,6 +1,6 @@
 import re
 
-from generate_ipa_section import generate_ipa_section
+from generate_ipa_section import word_to_ipa, ipa_to_celabric_word
 
 with open('index.html', 'r') as f:
     text = f.read()
@@ -12,4 +12,13 @@ for word in all_words:
     if re.search(f'id="word-{word}"', text) is None:
         missing_words.add(word)
 
-print(' '.join(sorted(missing_words)))
+alphabet = 'ieaouUOpbPtdTcjCkgKvfzsZSyhwxqQmnrl'
+alphabet_list = [ch for ch in alphabet]
+alphabet_dict = {ch: f'{i:02}' for i, ch in enumerate(alphabet_list)}
+
+def sorting_helper(word):
+    celabric_word = ipa_to_celabric_word(word_to_ipa(word))
+    number_word = ''.join([str(alphabet_dict[ch]) for ch in celabric_word])
+    return number_word
+
+print(' '.join(sorted(missing_words, key=sorting_helper)))
