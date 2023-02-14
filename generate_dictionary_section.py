814c7939b40e1cb3195a76b26a665154e2658a93
babdus <babdusi@gmail.com>
2023-02-14 04:16:32 +0400

    Add missing words in dictionary without definitions yet

--- generate_dictionary_section.py
+++ generate_dictionary_section.py
@@ -3,15 +3,17 @@ from generate_ipa_section import generate_ipa_section
 
 words = sys.argv[1:]
 
-for word in words:
-    sections = generate_ipa_section(word)[1]
+with open('out.txt', 'w') as f:
+    for word in words:
+        sections = generate_ipa_section(word)[1]
 
-    html = f'{" "*12}<li id="word-{word}">'
-    html += f'\n{" "*16}{sections["celabric"]}'
-    html += f' {sections["ipa"]}'
-    html += f'\n{" "*16}{sections["button"]}'
-    html += f'\n{" "*16}<ul>\n{" "*20}<li>'
-    html += f'\n{" "*24}<span class="grammar"></span>'
-    html += f'\n{" "*20}</li>\n{" "*16}</ul>\n{" "*12}</li>'
+        html = f'{" "*12}<li id="word-{word}">'
+        html += f'\n{" "*16}{sections["celabric"]}'
+        html += f' {sections["ipa"]}'
+        html += f'\n{" "*16}{sections["button"]}'
+        html += f'\n{" "*16}<ul>\n{" "*20}<li>'
+        html += f'\n{" "*24}<span class="grammar"></span>'
+        html += f'\n{" "*20}</li>\n{" "*16}</ul>\n{" "*12}</li>'
 
-    print(html)
+        f.write(html)
+        f.write('\n')
